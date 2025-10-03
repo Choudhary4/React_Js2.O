c1276443d579fa2cb58257ec2ecc9d769a9088c7
Saurabh Kumar <saurabhkuntal91@gmail.com>
2025-10-03 10:50:28 +0530

    revison of react hooks

---
+++ lecture01/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import {useState} from 'react'
+function App() {
+  const [counter,setCounter] = useState(23);
+   const addCounter = ()=>{
+      setCounter(counter+1)
+  }
+
+  const removeCounter = ()=>{
+    if(counter>0){
+        setCounter(counter-1);
+    }
+  
+  }
+
+  return (
+    <>
+     <div>
+      Counter is {counter}
+     </div>
+     <button onClick={addCounter}>Add Counter</button>
+     <br />
+     <button onClick={removeCounter}>Remove Counter</button>
+    </>
+  )
+}
+
+export default App
